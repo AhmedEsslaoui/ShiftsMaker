1f75d9e79c754d21e1dc29eb0d0c9bbda13eb3bd
AhmedEsslaoui <medslaoui14563@gmail.com>
2024-12-02 17:34:29 +0100

    Update next.config.js and GitHub Actions for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.GITHUB_PAGES ? '/ShiftsMaker' : '',
 }
 
 module.exports = nextConfig
